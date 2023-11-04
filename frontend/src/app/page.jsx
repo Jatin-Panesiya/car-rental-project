cddd5c009352199d2eb865bd525edfdb305b73d9
Admin <Admin@DESKTOP-CCNHMQU>
2023-11-04 10:24:20 +0530

    api generating

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,10 +2,35 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 function Home() {
+
+  const carData = [
+    {
+      id:'1',
+      carImages : {front:'',back:'',side:'',top:'',inside:'',full:'https://freepngimg.com/save/22421-bmw-x5-transparent-image/800x531'},
+      carInfo : { carName : 'BMW X5' ,doors : '5', sunroof:true, AirCondition : true, color:'black', seats:'7', testYear:'2003-2018', category:'Luxury'},
+      rentPrice : '',
+      availability:true,
+      vehicalCondition:'Good',
+      termsAndCondition:'',
+      additionalFeatures:'',
+      legalRequirements:'',
+      fuelType:'',
+      average:'12km/leter',
+      drivingType : 'automatic' 
+    }
+  ]
+
   return (
     <>
-    <Header/>
-    <h1>Home Page</h1>
+      <Header />
+      {
+        carData.map((data)=>{
+          <div key={data.id}>
+              <img src={data.carImages.full} alt="" />
+          </div>
+        })
+      }
+      <h1>Home Page</h1>
     </>
   )
 }
